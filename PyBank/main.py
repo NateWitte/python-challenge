9aadd3df9f28df12e5ee7700c99e01f76d1e48e4
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-01-21 17:50:25 -0600

    Added CSV file and read it into main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,11 @@
-print("Hello from PyBank")
+#Need to import os and csv
+import os
+import csv
+
+#Since it is already in our "PyBank" folder, we can just open it
+with open('budget_data.csv') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
