0c87c6d1ae8d9f1d82e28ee03c0b2b821f9f73b0
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-01-21 17:54:56 -0600

    Added a counter for months

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,8 @@
 import os
 import csv
 
+#Create variables
+rowcounter=0
 #Since it is already in our "PyBank" folder, we can just open it
 with open('budget_data.csv') as csvfile:
 
@@ -9,3 +11,8 @@ with open('budget_data.csv') as csvfile:
 
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        rowcounter=rowcounter+1
+
+    print(f"Number of rows: {rowcounter}")
