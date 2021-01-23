dafba0f60d483b5a0d0eed21a45099a51bef9441
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-01-23 01:47:36 -0600

    Added text file output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,6 +19,7 @@ with open('election_data.csv') as csvfile:
         else:
             votedcandidates[candidate]+=1
 
+#terminal output
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {votecount}")
@@ -37,3 +38,25 @@ for i in votedcandidates:
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
+
+#text output
+text_file = open("ElectionResults.txt", "w")
+text_file.write("Election Results\n")
+text_file.write("-------------------------\n")
+text_file.write(f"Total Votes: {votecount}\n")
+text_file.write("-------------------------\n")
+winner="None"
+mostvotes=0
+for i in votedcandidates:
+    if votedcandidates[i]>mostvotes:
+        mostvotes=votedcandidates[i]
+        winner = i
+    thiscandidate=i
+    votes = votedcandidates[i]
+    percentage = (votes/votecount)*100
+    percentage = round(percentage,3)
+    text_file.write(f"{i}: {percentage}% ({votedcandidates[i]})\n")
+text_file.write("-------------------------\n")
+text_file.write(f"Winner: {winner}\n")
+text_file.write("-------------------------\n")
+text_file.close()
