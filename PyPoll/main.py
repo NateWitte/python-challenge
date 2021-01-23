b81363a46ab18790626a4df1a7b3c7fb0c6d7e2b
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-01-23 01:21:28 -0600

    Added a list to keep track of voted for candidates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,7 @@ import csv
 
 #Variables
 votecount = 0
+votedcandidates = []
 with open('election_data.csv') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -13,4 +14,17 @@ with open('election_data.csv') as csvfile:
 
     for row in csvreader:
         votecount=votecount+1
+        candidate = row[2]
+        newcandidate = True
+        for people in votedcandidates:
+            if candidate == people:
+                newcandidate = False
+        
+        if newcandidate == True:
+            votedcandidates.append(candidate)
+
+for i in votedcandidates:
+    print(f"{i}")
+
+
 print(f"Total number of votes: {votecount}")
