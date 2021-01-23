3506935ed859270a63e1b09ef7a673038a07ef32
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-01-23 01:15:47 -0600

    Added vote count

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,9 +2,15 @@
 import os
 import csv
 
+#Variables
+votecount = 0
 with open('election_data.csv') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        votecount=votecount+1
+print(f"Total number of votes: {votecount}")
